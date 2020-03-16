ba58b6f8b0bfd665459a2928bed88de814902154
Reshika Samala <samala.reshika@gmail.com>
2020-03-16 16:28:59 -0700

    Fixed CORS issue

--- src/main/java/com/mytechladder/moviereview/controller/ReviewController.java
+++ src/main/java/com/mytechladder/moviereview/controller/ReviewController.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,8 @@ import com.mytechladder.moviereview.repository.MovieRepo;
 import com.mytechladder.moviereview.repository.ReviewRepo;
 import com.mytechladder.moviereview.repository.UserRepo;
 
+
+@CrossOrigin
 @RestController
 @RequestMapping("/movie")
 public class ReviewController {
